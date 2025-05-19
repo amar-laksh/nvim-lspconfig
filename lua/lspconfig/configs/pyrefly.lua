3380a5028d513ac6a0ee4b88299f54da49cd8cec
Amar Lakshya <amar.lakshya@protonmail.com>
2025-05-19 20:53:29 +0100

    feat(pyrefly): adds pyrefly configs

---
+++ lua/lspconfig/configs/pyrefly.lua
@@ -0,0 +1,28 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'pyrefly', 'lsp' },
+    filetypes = { 'python' },
+    root_markers = {
+      'pyrefly.toml',
+      'pyproject.toml',
+      'setup.py',
+      'setup.cfg',
+      'requirements.txt',
+      'Pipfile',
+      '.git',
+    }
+  },
+  docs = {
+    description = [[
+ https://pyrefly.org/
+
+`pyrefly`, a faster Python type checker written in Rust.
+
+ `pyrefly` is still in development, so please report any errors to
+ our issues page at https://github.com/facebook/pyrefly/issues.
+
+]],
+  },
+}
